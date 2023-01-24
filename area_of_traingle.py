f58312011341af2e143e2dd10de7c7aabe1ba770
Tejaswini <tejaswini.sp@thundersoft.com>
2023-01-24 19:08:08 +0530

    Python Day1 assignments

---
+++ area_of_traingle.py
@@ -0,0 +1,10 @@
+# Program to find area of traingle
+
+# Take the inputs
+
+base=int(input("Enter the base: "))
+height=int(input("enter the height: "))
+
+#Calculate the area of traingle
+area=(0.5 * (base * height))
+print(area)
